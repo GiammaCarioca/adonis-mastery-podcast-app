bb662e79ad30ad03f16a57bc02dedb7f69848486
Thiago Giammattey <giammattey@gmail.com>
2020-05-24 10:29:10 -0300

    authentication middleware

--- start/routes.js
+++ start/routes.js
@@ -32,22 +32,22 @@ Route.group(() => {
   Route.put('/account', 'UserController.updateAccount')
   Route.get('/password', 'UserController.showChangePassword').as('settings.password')
   Route.put('/password', 'UserController.updatePassword')
-}).prefix('/settings')
+}).prefix('/settings').middleware(['auth'])
 Route.resource('podcasts', 'PodcastController')
   .except(['index', 'show'])
   .validator(new Map([
     [['podcasts.store'], ['StorePodcast']],
     [['podcasts.update'], ['UpdatePodcast']]
-  ]))
-Route.get('my-podcast', 'UserController.myPodcast').as('myPodcast')
-Route.get('/subscriptions', 'UserController.subscriptions').as('subscriptions')
+  ])).middleware(new Map([[['create', 'store', 'update', 'destroy'], ['auth']]]))
+Route.get('my-podcast', 'UserController.myPodcast').as('myPodcast').middleware(['auth'])
+Route.get('/subscriptions', 'UserController.subscriptions').as('subscriptions').middleware(['auth'])
 Route.group(() => {
   Route.post('/', 'SubscriptionController.subscribe').as('subscriptions.store')
   Route.delete('/:id', 'SubscriptionController.unSubscribe').as('subscriptions.destroy')
-}).prefix('subscriptions')
+}).prefix('subscriptions').middleware(['auth'])
 
-Route.get('/:slug/episodes/create', 'EpisodeController.create').as('episodes.create')
-Route.post('/:slug/episodes', 'EpisodeController.store').as('episodes.store')
+Route.get('/:slug/episodes/create', 'EpisodeController.create').as('episodes.create').middleware(['auth'])
+Route.post('/:slug/episodes', 'EpisodeController.store').as('episodes.store').middleware(['auth'])
 Route.get('/:slug/episodes/:id', 'EpisodeController.download').as('episodes.download')
 
 Route.get('/categories/:slug', 'CategoryController.show').as('categories.show')
